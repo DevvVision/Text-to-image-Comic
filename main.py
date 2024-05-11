56b0bcbfb6c07d72ac9b851befe7fba981def693
Harshit Srivastava <167855220+DevvVision@users.noreply.github.com>
2024-05-11 14:53:02 +0530

    Update main.py
    just some change ,for handling image files better

--- main.py
+++ main.py
@@ -3,27 +3,26 @@ import io
 import numpy as np
 from PIL import Image
 import streamlit as st
-def AnimeGen(text):
+def comicgen(text):
     try:
-        image_bytes = A_query({
+        image_bytes = C_query({
             "inputs": text,
         })
-	    # Image.open
-        image = io.BytesIO(image_bytes)
-        # Convert PIL Image to bytes in PNG format
-        # image_png_bytes = io.BytesIO()
-        # image.save(image_png_bytes, format='PNG')
-        st.image(image, caption='Generated Image', use_column_width=True,width=600)
-        st.download_button(label="Download File",data = image,file_name="Comic.png")
+        image = Image.open(io.BytesIO(image_bytes))
+        image_byt = io.BytesIO(image_bytes)
+        print(type(image))
+        print(type(image_byt))
+        st.image(image, caption='Generated Image')
+        st.download_button(label="Download File", data=image_byt, file_name="Anime.jpeg")
     except Exception as e:
         print(e)
         st.error(f"Error occured while displaying the image {e}")
-def A_query(payload):
-	response = requests.post(API_URL[md_index], headers=headers, json=payload)
+def C_query(payload):
+	response = requests.post(C_API_URL[md_index], headers=headers, json=payload)
 	return response.content
 
 
-API_URL = ["https://api-inference.huggingface.co/models/ogkalu/Comic-Diffusion","https://api-inference.huggingface.co/models/GraydientPlatformAPI/comicbabes2","https://api-inference.huggingface.co/models/Stelath/textual_inversion_comic_strip_turbo"]
+C_API_URL = ["https://api-inference.huggingface.co/models/ogkalu/Comic-Diffusion","https://api-inference.huggingface.co/models/GraydientPlatformAPI/comicbabes2","https://api-inference.huggingface.co/models/Stelath/textual_inversion_comic_strip_turbo"]
 headers = {"Authorization": "Bearer "+st.secrets["API-KEY"]}
 
 st.set_page_config(
@@ -37,9 +36,6 @@ st.set_page_config(
     }
 )
 st.write("# **Text to Image Generator**:wave:")
-# prompt = st.text_input(label="Enter your prompt here")
-
-
 st.write("## Comic")
 mdl_Comic = st.radio(" **Select the model** ",["Comic-Diffusion","comicbabes2","textual_inversion_comic_strip_turbo"],key="Model-selection-1")
 check1 = st.button(label="Submit", key="Model-selection-4")
@@ -51,4 +47,4 @@ if check1==True:
     md_index=1
   elif mdl_Comic== "textual_inversion_comic_strip_turbo":
     md_index=2
-  AnimeGen(prompt)
+  comicgen(prompt)
